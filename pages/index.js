521aa44764656076c0f9598960edb7e318d23525
edneuzacn <edneuza.nascimento@yahoo.com.br>
2025-10-08 20:21:47 -0300

    Filmes0810

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,27 @@
+import { filmes }from "../data/filmes.js"
+
 function Home() {
+    console.log(filmes)
+    
+    const primeiroFilme = filmes[0]
+
+    const estiloImagem = {
+        width:"300px"
+    };
+
     return <>
     return <h1>Filmes</h1>
+
+    <ul>
+        <li>
+            <p>Nome do filme</p>
+            <p>{ primeiroFilme.titulo}</p>
+              <p>{ primeiroFilme.ano}</p>
+                <p>{ primeiroFilme.genero}</p>
+                  <p>{ primeiroFilme.nota}</p>
+                  <img style={estiloImagem} src={primeiroFilme.poster}/>
+        </li>
+    </ul>
     <p>lista de filmes aqui</p>
         </>  
 }
