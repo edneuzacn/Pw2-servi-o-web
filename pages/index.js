c868637ba38688245693e8e6ee960b92b4e3f4bf
edneuzacn <edneuza.nascimento@yahoo.com.br>
2025-10-20 22:06:20 -0300

    commit salvo

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,46 @@
-import { filmes }from "../data/filmes.js"
+import { useLinkStatus } from "next/link.js";
+import { filmes }from "../data/filmes.js"//importar o array de filmes
 
-function Home() {
-    console.log(filmes)
-    
-    const primeiroFilme = filmes[0]
+function CardFilme({filme}) {
+    const estiloImagem = {//objeto de estilo para a imagem  
+        width:"300px"//definir a largura da imagem
+    };//fechar o objeto de estilo
 
-    const estiloImagem = {
-        width:"300px"
-    };
+    return(
+         <li key={`card-filme-${filme.id}`}> 
+                                <p>Título:{ filme.titulo}</p>
+                                <p> Ano:{ filme.ano}</p>
+                                <p> Gênero:{ filme.genero}</p>
+                                <p>Nota:{ filme.nota}</p>
+                                <img style={estiloImagem} src={filme.poster}/>
+                            </li>
+    )
+}
+function Home() {//componente Home
 
     return <>
-    return <h1>Filmes</h1>
+    return <h1>Filmes</h1>//título da página
 
-    <ul>
-        <li>
+    <ul>//lista de filmes 
+        <li>//item da lista
             <p>Nome do filme</p>
-            <p>{ primeiroFilme.titulo}</p>
-              <p>{ primeiroFilme.ano}</p>
-                <p>{ primeiroFilme.genero}</p>
-                  <p>{ primeiroFilme.nota}</p>
-                  <img style={estiloImagem} src={primeiroFilme.poster}/>
+            <ul>
+                {
+                    filmes.map(( filme) => {
+                        return(
+                            <CardFilme filme={filme} key={filme.id}/>
+
+                            
+                           
+                        );
+                    })
+                }    
+            </ul>
+                
         </li>
     </ul>
     <p>lista de filmes aqui</p>
         </>  
 }
-export default Home;
+export default Home;//exportar o componente Home como padrão
    
